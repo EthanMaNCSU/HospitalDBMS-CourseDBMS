40bc81f94fbf72b38721361e2292458f4f0ec842
DESKTOP-N5C18FQ\Yifan Zhao <mistersavagezyf@gmail.com>
2019-03-12 20:36:21 -0400

    Create tables

--- src/CreateTable.java
+++ src/CreateTable.java
@@ -31,9 +31,28 @@ public class CreateTable {
 		try {
 			connectToDatabase();
 
-			statement.executeUpdate("CREATE TABLE Students (Name VARCHAR(20), "
-					+ "School VARCHAR(10), Age INTEGER, FundingReceived INTEGER, Income INTEGER, Sex CHAR(1))");
+			statement.executeUpdate("CREATE TABLE Patients (PID INT, SSN VARCHAR(9), Name VARCHAR(40) NOT NULL, DOB VARCHAR(8) NOT NULL, Gender VARCHAR(1) NOT NULL, " +
+					"Age INT NOT NULL, Status VARCHAR(64) NOT NULL, Address VARCHAR(64), PhoneNum VARCHAR(10) NOT NULL, Primary Key(PID))");
+			statement.executeUpdate("CREATE TABLE Wards (WNum INT, SID INT, Capacity INT BOT NULL, ChargePerDay FLOAT NOT NULL, " +
+					"Primary Key(WNum), Foreign Key(SID) references Staff(SID))");
+			statement.executeUpdate("CREATE TABLE Staff (SID INT, Name VARCHAR(40) NOT NULL, Age INT NOT NULL, Gender VARCHAR(1) NOT NULL, JobTitle VARCHAR(20) NOT NULL, " +
+					"ProfTitle VARCHAR(20), Department VARCHAR(20), Address VARCHAR(64) NOT NULL, PhoneNum VARCHAR(10) NOT NULL, Type VARCHAR(10), " +
+					"Primary Key(SID, Type))");
+			statement.executeUpdate("CREATE TABLE BillingAccounts (PID INT, BillingAddr VARCHAR(64) NOT NULL, CreatedDate VARCHAR(8) NOT NULL, " +
+					"Balance FLOAT NOT NULL DEFAULT 0.0, Primary Key(PID), Foreign Key(PID) refrences Patients(PID))");
+			statement.executeUpdate("CREATE TABLE BillingRecords (RID INT, PID INT, RecordType VARCHAR(20) NOT NULL, CreatedDate VARCHAR(8) NOT NULL, " +
+					"Primary Key(RID), Foreign Key(PID) references Patients(PID))");
+			statement.executeUpdate("CREATE TABLE PaymentMethods (PMID INT, PID INT, SID INT, PMName VARCHAR(20) NOT NULL, " +
+					"CardNum VARCHAR(16) NOT NULL, CardValidDate VARCHAR(4) NOT NULL), Primary Key(PMID, PID, SID), Foreign Key(PID) references Patients(PID), Foreign Key(SID) references Staff(SID))");
+			statement.executeUpdate("CREATE TABLE MedicalRecord (MID INT, PID INT, SID INT, StartDate VARCHAR(8) NOT NULL, EndDate VARCHAR(8) NOT NULL, " +
+					"Prescription VARCHAR(256), Diagnosis VARCHAR(256)), Primary Key(MID, PID, SID), Foreign Key(PID) references Patients(PID), Foreign Key(SID) references Staff(SID)");
+			statement.executeUpdate("CREATE TABLE CheckIn (PID INT, WNum VARCHAR(20) NOT NULL, StartDate VARCHAR(8) NOT NULL, " +
+					"EndDate VARCHAR(8) NOT NULL, BedNum VARCHAR(20) NOT NULL, Primary Key(PID, WNum), Foreign Key(WNum) references Patients(PID))");
 
+			statement.executeUpdate("CREATE TABLE TreatedBy (PID INT, SID INT, Primary Key(PID, SID))");
+
+					//+ "School VARCHAR(10), Age INTEGER, FundingReceived INTEGER, Income INTEGER, Sex CHAR(1))");
+/*
 			statement.executeUpdate("INSERT INTO Students VALUES ('Todd', 'NC State'," + " 18, 16000, 30000, 'M')");
 			statement.executeUpdate("INSERT INTO Students VALUES ('Max', 'Stanford'," + " 21, 20000, 70000, 'M')");
 			statement.executeUpdate("INSERT INTO Students VALUES ('Alex', 'UNC'," + " 19, 8000, 40000, 'M')");
@@ -49,6 +68,7 @@ public class CreateTable {
 			statement.executeUpdate("INSERT INTO Schools VALUES ('Harvard', 'Massachusetts', 50000, 38000)");
 			statement.executeUpdate("INSERT INTO Schools VALUES ('UCLA', 'California', 36000, 30000)");
 			statement.executeUpdate("INSERT INTO Schools VALUES ('NYU', 'New York', 22000, 41000)");
+*/
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (SQLException e) {
